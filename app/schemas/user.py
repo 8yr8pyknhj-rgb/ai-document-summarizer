b42d5c5218ed13011cb567efa5cbb24953fff227
8yr8pyknhj-rgb <8yr8pyknhj@privaterelay.appleid.com>
2026-07-30 18:54:44 +0100

    Create user.py

---
+++ app/schemas/user.py
@@ -0,0 +1,23 @@
+from pydantic import BaseModel
+from pydantic import EmailStr
+
+
+class UserCreate(BaseModel):
+    full_name: str
+    email: EmailStr
+    password: str
+
+
+class UserLogin(BaseModel):
+    email: EmailStr
+    password: str
+
+
+class UserResponse(BaseModel):
+    id: str
+    full_name: str
+    email: EmailStr
+
+    model_config = {
+        "from_attributes": True
+    }
