29f8c17aff37d105a60d4c3fe7e6ce8d689c05ad
8yr8pyknhj-rgb <8yr8pyknhj@privaterelay.appleid.com>
2026-07-30 18:50:43 +0100

    Update users.py

--- app/models/users.py
+++ app/models/users.py
@@ -1,1 +1,34 @@
+import uuid
 
+from sqlalchemy import String
+from sqlalchemy import Boolean
+
+from sqlalchemy.orm import Mapped
+from sqlalchemy.orm import mapped_column
+
+from app.db.database import Base
+
+
+class User(Base):
+    __tablename__ = "users"
+
+    id: Mapped[str] = mapped_column(
+        String,
+        primary_key=True,
+        default=lambda: str(uuid.uuid4())
+    )
+
+    full_name: Mapped[str] = mapped_column(String(255))
+
+    email: Mapped[str] = mapped_column(
+        String(255),
+        unique=True,
+        index=True
+    )
+
+    password_hash: Mapped[str] = mapped_column(String)
+
+    is_active: Mapped[bool] = mapped_column(
+        Boolean,
+        default=True
+    )
